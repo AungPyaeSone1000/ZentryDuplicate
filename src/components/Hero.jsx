577504f1c3b95090339cbc8b6187de885c54e0f8
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-11-25 20:03:51 -0500

    First commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,121 @@
+import { useState, useRef } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVids, setLoadedVids] = useState(0);
+
+  const totalVids = 3;
+  const nextVid = useRef(null);
+
+  const upcomingVidIndex = (currentIndex % totalVids) + 1;
+
+  const handleVidClick = () => {
+    setHasClicked(true);
+    setCurrentIndex(upcomingVidIndex);
+  };
+
+  const handleVidLoad = () => {
+    setLoadedVids((prev) => prev + 1);
+  };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVidRef.current.play(),
+        });
+        gsap.from('#current-video',{
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: 'power1.inOut'
+        })
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  const getVidSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cusor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleVidClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVid}
+                src={getVidSrc(upcomingVidIndex + 1)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVidLoad}
+              ></video>
+            </div>
+          </div>
+          <video
+            ref={nextVid}
+            src={getVidSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolut z-20 size-64 object-cover object-center"
+            onLoadedData={handleVidLoad}
+          ></video>
+          <video
+            src={getVidSrc(currentIndex === totalVids - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+          ></video>
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the meta layer
+              <br />
+              Unleaseh the beast economy
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass={"!bg-yellow-300 flex-center gap-1"}
+            />
+          </div>
+        </div>
+      </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
+    </div>
+  );
+};
+export default Hero;
